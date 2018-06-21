d5b6e9431f15c3a1822a0e5a8ee072a3fd192d06
Amy Thompson <amythompson9@gmail.com>
2018-06-20 20:26:48 -0400

    Arguments

--- UdemyJSFunctions/JavaScript.js
+++ UdemyJSFunctions/JavaScript.js
@@ -1,8 +1,18 @@
-﻿function singSong() {
-    console.log("Twinkle, twinkle, little star,");
-    console.log("How I wonder what you are!");
-    console.log("Up above the world so high,");
-    console.log("Like a diamond in the sky.");
-}
+﻿//function singSong() {
+//    console.log("Twinkle, twinkle, little star,");
+//    console.log("How I wonder what you are!");
+//    console.log("Up above the world so high,");
+//    console.log("Like a diamond in the sky.");
+//}
 
-singSong();
+//singSong();
+
+//function sayHello() {
+//    console.log("Hello there!");
+//}
+
+//function sayHello(name) {
+//    console.log("Hello there " + name + "!");
+//}
+
+//sayHello("Rusty");
